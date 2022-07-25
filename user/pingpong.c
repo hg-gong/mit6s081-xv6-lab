b1c53de1f0711481164c5a840be3fbed64684252
larry <larry@qq.com>
2022-07-25 17:25:18 +0800

    lab1, pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,32 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main()
+{
+    int pid;
+    int fds1[2];
+    int fds2[2];
+    char buf[512];
+
+    pipe(fds1);
+    pipe(fds2);
+
+    pid = fork();
+
+    if(pid == 0){
+        printf("child\n");
+        read(fds1[0], buf, sizeof(buf));
+        printf("%d: received ping\n", getpid());
+        write(fds2[1], "pong\n",5);
+    } else {
+        printf("parent\n");
+        write(fds1[1], "ping\n",5);
+        read(fds2[0], buf, sizeof(buf));
+        printf("%d: received pong\n", getpid());
+    }
+  
+    exit(0);
+    
+}
